b96d4483a9a7b316d648c1730686a0df2c8ea3ef
WeboxTV <59010081+WeboxTV@users.noreply.github.com>
2020-03-26 13:09:03 +0700

    Update search.js

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ function myFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("horizontal-list");
+  ul = document.getElementById("myMenu");
   li = ul.getElementsByTagName("li");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
